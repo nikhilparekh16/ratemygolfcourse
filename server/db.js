eb2b2cf5c5b8f72c60b46cf6d1440cbc48048644
Nikhil Parekh <nikhilparekh16@gmail.com>
2025-07-16 19:07:43 -0400

    db connected

---
+++ server/db.js
@@ -0,0 +1,15 @@
+// server/db.js
+import pkg from 'pg';
+const { Pool } = pkg;
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required by Supabase
+  }
+});
+
+export default pool;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import pool from "./db.js";
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to RateMyGolfCourse API");
+app.get("/", (req, res) =>{
+    res.send("Welcome to the RateMyGolfCourse API");
 });
 
-const PORT = process.env.PORT || 5001;
+app.get("/api/courses", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM courses");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
